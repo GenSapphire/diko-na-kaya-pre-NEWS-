26a583c93db2362a36b3fa32f9d3c8b4a9bef13a
GenSapphire <justinjaydonio24@gmail.com>
2024-12-04 05:08:56 +0800

    Add files via upload

---
+++ KyusiEsports/!News.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kyusi Esports</title>
+    <link rel="stylesheet" href="css/QCEnews.css">
+</head>
+
+<body>
+<div class="wrapper">
+        <!-- Navbar -->
+        <?php include "#Navbar.php"; ?>
+
+        <!-- About Us Main Body -->
+    <main>
+    <p class="titlenews">N<span class="E">E</span>WS</p>
+
+    <div class="item2">
+        <div class="hover-container"> <!-- New hover container -->
+            <p class="nit1">INFINIX UNIVERSITY LEAGUE TOURNAMENT<br>
+            SEASON 1</p>
+
+            <div class="news-items1">
+                <img src="qceimages/a2.jpg" alt="Tournament Image">
+                <img style="width: 40%" src="qceimages/a1.jpg" alt="Second Tournament Image">
+            </div>
+
+            <p class="nit1-content">KE Tiddles, MLBB Team representatives from the 
+                Quezon City University competed on an intense battle against 
+                PUP - Pylon Mittsu on June 29, 2024 at SM Megamall and claimed the 
+                first place for their first time competing on a big stage</p>
+        </div> <!-- End of hover-container -->
+
+        <div class="hover-container">
+        <p class="nit2">INFINIX 3V3 COMMUNITY TOURNAMENTS</p>
+        <div class="news-items2">
+            <img src="qceimages/c1.jpg" alt="Group Image">
+            <p class="nit2-content">Kyusi Esports Community competed on a 3v3 
+            battle last July 12, 2024 at the Infinix Concept Store at SM 
+            North Edsa, Annex Bldg. Few of the contestants from the community 
+            won the championship with a smart watch as their prize.</p>
+        </div>
+        </div>
+
+        <div class="hover-container">
+        <p class="nit3">Freshie Walk</p>
+        <div class="news-items3">
+            <img src="qceimages/d1.jpg" alt="Tao Image">
+            <img src="qceimages/d2.jpg" alt="Ufo Image">
+        </div>
+        <p class="nit3-content">KE Tiddles, MLBB Team representatives from the 
+            Quezon City University competed on an intense battle against 
+            PUP - Pylon Mittsu on June 29, 2024 at SM Megamall and claimed the 
+            first place for their first time competing on a big stage</p>
+        </div>
+
+        <div class="hover-container">
+        <p class="nit4">Astreus</p>
+        <div class="news-items4">
+            <img src="qceimages/e1.jpg" alt="Group Image">
+            <p class="nit4-content">KE Astreus, the League of 
+                Legends: Wildrift representatives of the Kyusi Esports Community 
+                competed on both AkoSi Dogie’s #DayoSerye and AcadArena’s 
+                October Aces: Wildrift last October 28 - 30, 2024.</p>
+        </div>
+        </div>
+
+        <div class="hover-container">
+        <p class="nit5">INFINIX UNIVERSITY LEAGUE TOURNAMENT SEASON 2</p>
+        <div class="news-items5">
+            <img src="qceimages/f1.jpg" alt="Tahh Image">
+            <img style="width: 45%" src="qceimages/f2.png" alt="Second Tournament Image">
+        </div>
+        <p class="nit5-content">KE Tiddles, MLBB Team representatives from the 
+            Quezon City University competed on an intense battle against 
+            PUP - Pylon Mittsu on June 29, 2024 at SM Megamall and claimed the 
+            first place for their first time competing on a big stage</p>
+
+        </div>
+
+
+
+
+
+    </div>
+
+    </main> 
+        
+        <!-- Footer -->
+        <?php include "#Footer.php"; ?>
+
+</div>
+
+    <!-- External JavaScript -->
+    <script src="javascript/QCE.js"></script>
+</body>
+
+</html>
